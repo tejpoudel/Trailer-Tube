f9bfaf0133afc1079bac71a43afb3067cd379f7b
Rohit Chhetri <rohit.chhetri@gmail.com>
2018-11-08 18:47:14 -0800

    validation, not found -app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,192 +1,217 @@
-$(Document).ready(function(){
+$(Document).ready(function () {
 
     //hiding movieinfo
     $("#trailerInfo").hide();
     //hiding Trailer
     $("#trailer").hide();
- 
-    
-    
+
+
+
     //alert("Welcome TO Trailer Tube");
     // Project - Trailer Tube
     //TODO Movie OMDb API
     //TODO Youtube API Working 
-    
-    
+
+
     //Api Information -
     //Youtube KEY = AIzaSyCb3e79c57_B7L6m214mksu6V3b_k_xEtA
     //OMDb Key - 388edf5a
-    
+
     //Get the input from id="movieSearch" input type and feed into t and store the input value to searchMovie Variable 
-    
+
     //var movieSearchArr = ["The Rock"];//$("#movieSearch").val().trim();
     //console.log(movieSearchArr);
-    
-    
-    
+
+
+
     //When id="searchButton" is clicked, It will take value from id=moviesearch and append to queryURL 
     //var movieSearch = "";
     function movieinfo() {
-            var movieSearch = $("#movieSearch").val().trim();
-    
-            var movRating = 10;
-    
-            var queryURL = "http://www.omdbapi.com/?t="+movieSearch+"&apikey=388edf5a";
-            console.log(queryURL);
-            // Creating an AJAX call for the specific movie 
-            $.ajax({
-              url: queryURL,
-              method: "GET"
-            }).then(function(response) {
+        var movieSearch = $("#movieSearch").val().trim();
+
+        var movRating = 10;
+
+        var queryURL = "http://www.omdbapi.com/?t=" + movieSearch + "&apikey=388edf5a";
+        console.log(queryURL);
+        // Creating an AJAX call for the specific movie 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
             //console.log(response);
-    
+
             //var movie Plot 
             var moviePlot = response.Plot;
             //console.log(moviePlot);  
-            
+
             //var moviePoster
             var moviePoster = response.Poster;
             //console.log(moviePoster);
-    
+
             //var movieTitle 
             var movieTitle = response.Title;
             //console.log(movieTitle);
-    
+
             // var movieDirector 
             var movieDirector = response.Director;
             //console.log(movieDirector);
-    
+
             //var Actors 
             var movieActors = response.Actors;
             //console.log(movieActors);
-    
+
             //var Ratings 
             var movieRating = response.imdbRating;
+
             console.log(movieRating);
-    
+            if (movieRating === undefined) {
+                alert("Movie not found!");
+                return false
+            }
+
+           
+
             //var movieReleased 
             var movieReleased = response.Released;
-            console.log(movieReleased); 
-    
+            console.log(movieReleased);
+
             //Div trailerInfo
-    
+
             //var movieInfo = $("<div id='movieInfo'>");
             var movieInfo = $("#trailerInfo");
             //Title Information 
-            var Title = $(".title").text("Movie: " +movieTitle);
-    
+            var Title = $(".title").text("Movie: " + movieTitle);
+
             // Img 
             var imgMovie = $(".mvImg").attr("src", moviePoster);
             movieInfo.append(imgMovie);
 
             //Plot information 
             movieInfo.append(Title);
-            
+
             //Movie Name
             var Plot = $(".overview").text("Overview: " + moviePlot);
             movieInfo.append(Plot);
-            
+
             // Rating information
-            var Rating = $(".rating").text("Movie Rating: " + movieRating +"/"+movRating);
+            var Rating = $(".rating").text("Movie Rating: " + movieRating + "/" + movRating);
             //console.log(Rating);
             movieInfo.append(Rating);
-            
+
             // Actors Information 
-            var Actors = $(".actors").text("Actors: "+movieActors);
+            var Actors = $(".actors").text("Actors: " + movieActors);
             movieInfo.append(Actors);
-    
+
             // Director 
             var director = $(".director").text("Director: " + movieDirector);
             movieInfo.append(director);
-    
+
             // Released 
-    
-            var realeased = $(".release").text("Released: "+ movieReleased);
+
+            var realeased = $(".release").text("Released: " + movieReleased);
             movieInfo.append(realeased);
-    
+
             var trailerBtn = $(".button1");
             movieInfo.append(trailerBtn);
-    
+
             $("#trailerInfo").append(movieInfo);
             //$("#trailer").append(movieTrailer);
 
-            }  
-    )};
+        }
+        )
+    };
     //movieinfo();
-    
-    function youtube(){
-    
+
+    function youtube() {
+
         var movieSearch = $("#movieSearch").val().trim();
-        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+movieSearch+"Official"+"Trailer"+"&type=video&key=AIzaSyCb3e79c57_B7L6m214mksu6V3b_k_xEtA";
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + movieSearch + "Official" + "Trailer" + "&type=video&key=AIzaSyCb3e79c57_B7L6m214mksu6V3b_k_xEtA";
         console.log(queryURL);
         //Creating an Ajax call for the specific trailer 
-        
+
         $.ajax({
-    
-            url:queryURL,
+
+            url: queryURL,
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response);
-    
+
             var videoID = response.items[0].id.videoId;
             console.log("Video Id :" + videoID);
-    
-            var movieTrailer = $("<iframe id=ytplayer type=text/html width=100% height=405 src=https://www.youtube.com/embed/"+videoID+" frameborder=0 allowfullscreen>");
+
+            var movieTrailer = $("<iframe id=ytplayer type=text/html width=100% height=405 src=https://www.youtube.com/embed/" + videoID + " frameborder=0 allowfullscreen>");
             console.log(movieTrailer);
-    
+
             $("#trailer").append(movieTrailer);
-    
+
             //$(trailer).append(movieTrailer);
         }
-    )};
-    
+        )
+    };
+
     //youtube(); 
-    
+
     // id="searchButton" is click and take input value from id="movieSearch" input type
     // Creating function 
-    
-    $("#searchButton").on("click", function(event){
-    
-    
-        //$("#trailerInfo").empty();
-        //$("#trailer").empty();
 
-        //Below code prevents to refresh when pressed enter 
-        event.preventDefault();
-    
-        if(document.getElementById("movieSearch").value == ""){
-            alert("Please Enter your movie name!");
-            return false ;
-        } else{
-                
-           // add above movieSearch value into array 
-           // movieSearchArr.push(movieSearch);
-           var movieSearch = $("#movieSearch").val().trim();
-           console.log(movieSearch);
-           
-           
-        }
-        
-        movieinfo();
-        $("#trailerInfo").show();
-        //$("#trailer").show();
-    
-        //hiding Trailer
-         $("#trailer").hide();
-        
-        youtube(); 
-        $("#trailer").empty();
-    });
+    $("#searchButton").on("click", function (event) {
 
-    //Watch Tailer Code 
+        var queryURL = "http://www.omdbapi.com/?t=" + movieSearch + "&apikey=388edf5a";
+        console.log(queryURL);
+        // Creating an AJAX call for the specific movie 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //console.log(response);
 
-     $(".button1").on("click", function(event){
+            //var movieTitle 
+            var movieRating = response.Rating;
+            console.log(movieRating);
 
-         //alert("Hello I am clicked !")
-          $("#trailer").show();
-        //   $("#trailer").append(movieTrailer);
-          
+            //$("#trailerInfo").empty();
+            //$("#trailer").empty();
+
+            //Below code prevents to refresh when pressed enter 
+            event.preventDefault();
+
+            if (document.getElementById("movieSearch").value == "") {
+                alert("Please Enter your movie name!");
+                return false;
+            } else if (movieRating === "undefined") {
+                alert("Movie not found!");
+
+            } else {
+
+                // add above movieSearch value into array 
+                // movieSearchArr.push(movieSearch);
+                var movieSearch = $("#movieSearch").val().trim();
+                console.log(movieSearch);
+
+
+            }
+
+
+            movieinfo();
+            $("#trailerInfo").show();
+            //$("#trailer").show();
+
+            //hiding Trailer
+            $("#trailer").hide();
+
+            youtube();
+            $("#trailer").empty();
+        });
+
+        //Watch Tailer Code 
+
+        $(".button1").on("click", function (event) {
+
+            //alert("Hello I am clicked !")
+            $("#trailer").show();
+            //   $("#trailer").append(movieTrailer);
+
+        })
+
     })
-
-    })
-    
+})
